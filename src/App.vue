d9faa41553eab930e1ac2510d0600bfd576ddd3a
Jacob McMichael <jacobmcmichael@gmail.com>
2022-09-18 17:14:35 -0700

    Add css reset, favicon, and Google Fonts.
    Signed-off-by: Jacob McMichael <jacobmcmichael@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,68 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <About/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import About from "@/pages/About";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    About,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+/* Import Google Fonts */
+@import url('https://fonts.googleapis.com/css2?family=Overlock:wght@900&family=PT+Sans:wght@700&family=Signika:wght@300&display=swap');
+
+/* Credit: https://www.joshwcomeau.com/css/custom-css-reset/ */
+/* 1. Use a more-intuitive box-sizing model. */
+*, *::before, *::after {
+  box-sizing: border-box;
+}
+
+/* 2. Remove default margin */
+* {
+  margin: 0;
+}
+
+/* 3. Allow percentage-based heights in the application */
+html, body {
+  height: 100%;
+}
+
+/* Typographic tweaks!
+  4. Add accessible line-height
+  5. Improve text rendering */
+body {
+  line-height: 1.5;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+/* 6. Improve media defaults */
+img, picture, video, canvas, svg {
+  display: block;
+  max-width: 100%;
+}
+
+/* 7. Remove built-in form typography styles */
+input, button, textarea, select {
+  font: inherit;
+}
+
+/* 8. Avoid text overflows */
+p, h1, h2, h3, h4, h5, h6 {
+  overflow-wrap: break-word;
+}
+
+/* 9. Create a root stacking context */
+#root, #__next {
+  isolation: isolate;
+}
+
+#app {
+  background-color: ;
 }
 </style>
---
+++ src/pages/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>Hello World</div>
+  <LogoComponent/>
+</template>
+
+<script>
+import LogoComponent from "@/components/icons/Logo";
+
+export default {
+  name: "AboutPage",
+  components: {
+    LogoComponent,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
