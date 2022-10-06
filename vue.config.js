7d6d071449d8fee41e8e43fad1e2e6695c8180af
Jacob McMichael <jacobmcmichael@gmail.com>
2022-10-05 17:46:50 -0700

    Amend publicPath to "/" root
    Signed-off-by: Jacob McMichael <jacobmcmichael@gmail.com>

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("@vue/cli-service");
 
 /* Sharing Global SCSS Variables via Vue CLI: https://stackoverflow.com/a/50772202/9604683 */
 module.exports = defineConfig({
-    publicPath: process.env.NODE_ENV === "production" ? "/jamcmich.github.io/" : "/",
+    publicPath: "/",
     transpileDependencies: true,
     css: {
         loaderOptions: {
